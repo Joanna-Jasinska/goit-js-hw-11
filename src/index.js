9ae7f0b417b81193d8f005205d7d3a7a637c7bda
Joanna <jjasinska.coding@gmail.com>
2023-04-04 14:44:55 +0200

    now getting images from server

--- src/index.js
+++ src/index.js
@@ -2,10 +2,34 @@
 import { inputChange } from './js/inputChange';
 import { sendData } from './js/inputChange';
 import { debounce } from './js/functions';
+import SimpleLightbox from 'simplelightbox';
+import { gallery } from './js/displayingGallery';
 
 const input = document.querySelector('.searchInput');
 debounce(input.focus.bind(input))(); //showing search bar after a delay
 //!!!aaa!!! inputChange should be fixed with proper font parameters
 input.addEventListener('input', () => inputChange(input)); //changing input width
-//!!!aaa!!! nothing is send to server
 input.addEventListener('input', sendData(input)); //sending req to server
+
+const options = {
+  captions: true,
+  captionsData: 'alt',
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+};
+
+let galleryDisplay = new SimpleLightbox('.gallery a', options);
+// galleryDisplay.on('show.simplelightbox', function () {
+//   // Do something…
+
+// });
+
+// galleryDisplay.on('error.simplelightbox', function (e) {
+//   console.log(e); // Some usefull information
+// });
+
+input.value = 'pink flowers';
+sendData(input)();
--- src/js/displayingGallery.js
+++ src/js/displayingGallery.js
@@ -3,6 +3,53 @@ import { log } from './functions';
 
 export const gallery = {
   clear: function () {
+    const galleryDiv = document.querySelector('.gallery');
+    galleryDiv.innerHTML = '';
     log('gallery cleared.');
+  },
+  renderCard: function (data) {
+    // adding one image card
+    log(`Rendering card, ID: ${data.id}`);
+    const markup = `
+    <div class="card">
+      <a href="${data.largeImageURL}"
+        ><img class="card__img" src="${data.previewURL}" alt="" title=""
+      /></a>
+      <div class="card__info">
+        <div class="card__info--entry">
+          <span class="span--description">Likes</span>
+          <span class="likes span--number">${data.likes}</span>
+        </div>
+        <div class="card__info--entry">
+          <span class="span--description">Views</span>
+          <span class="views span--number">${data.views}</span>
+        </div>
+        <div class="card__info--entry">
+          <span class="span--description">Comments</span>
+          <span class="comments span--number">${data.comments}</span>
+        </div>
+        <div class="card__info--entry">
+          <span class="span--description">Downloads</span>
+          <span class="downloads span--number">${data.downloads}</span>
+        </div>
+      </div>
+    </div>`;
+    const galleryDiv = document.querySelector('.gallery');
+    galleryDiv.innerHTML += markup;
+    const links = document.querySelectorAll('.gallery a');
+    const a = links ? links[links.length - 1] : null;
+    if (a) {
+      log(`preventing link ${a} from page redirecting`);
+      log(a);
+      a.onclick = e => {
+        e.preventDefault();
+      };
+    }
+  },
+  renderCards: function (list) {
+    //adding all cards
+    for (const card of list) {
+      this.renderCard(card);
+    }
   },
 };
