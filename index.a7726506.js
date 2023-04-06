e4ae3fa47c5f38aa4fafe9ae35952f2f3c9fa2eb
Joanna-Jasinska <111195576+Joanna-Jasinska@users.noreply.github.com>
2023-04-06 16:17:58 +0000

    Deploying to gh-pages from @ Joanna-Jasinska/goit-js-hw-11@f566bc0c50e28ab1d4f15a32cf58927f45c8108b 🚀

--- index.a7726506.js
+++
@@ -1,2 +0,0 @@
-var font="bold 12pt arial",inputMinWidth=160,inputMaxWidth=600,DEBOUNCE_DELAY=400;
-//# sourceMappingURL=index.a7726506.js.map
---
+++ index.f9480ad3.js
@@ -0,0 +1,2 @@
+var font="bold 12pt arial",inputMinWidth=160,inputMaxWidth=600,DEBOUNCE_DELAY=400,IMAGES_PER_PAGE=20;
+//# sourceMappingURL=index.f9480ad3.js.map
