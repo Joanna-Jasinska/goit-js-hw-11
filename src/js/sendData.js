0d739963245224df4b26d24b33aba3fc71de0e56
Joanna <jjasinska.coding@gmail.com>
2023-04-11 18:35:28 +0200

    async await

--- src/js/sendData.js
+++ src/js/sendData.js
@@ -5,7 +5,10 @@ import { gallery } from './displayingGallery';
 
 Notify.init({ showOnlyTheLastOne: true });
 
-export const sendData = (txt, { resetSearch } = { resetSearch: true }) => {
+export const sendData = async (
+  txt,
+  { resetSearch } = { resetSearch: true }
+) => {
   // element.value
   const req = txt.trim().replaceAll(' ', '+');
   if (req) {
@@ -13,55 +16,46 @@ export const sendData = (txt, { resetSearch } = { resetSearch: true }) => {
       lastPage = 0;
     }
     lastPage++;
-    fetchPictures(req)
-      .then(function (response) {
-        // handle success
-        maxPages = parseInt(
-          Math.ceil(response.data.totalHits / IMAGES_PER_PAGE)
+    try {
+      const response = await fetchPictures(req);
+      // .then(function (response) {
+      // handle success
+      maxPages = parseInt(Math.ceil(response.data.totalHits / IMAGES_PER_PAGE));
+      if (resetSearch) {
+        gallery.clear();
+      }
+      const loadMoreBtn = document.querySelector('.loadMoreBtn');
+      const footerContent = document.querySelector('.footer__content');
+      const header = document.querySelector('.header');
+      if (lastPage * IMAGES_PER_PAGE >= response.data.totalHits) {
+        loadMoreBtn.classList.add('hide');
+        footerContent.classList.add('hide');
+        header.classList.add('alwaysShow');
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
         );
-        if (resetSearch) {
-          gallery.clear();
+      } else {
+        if (loadMoreBtn.classList.contains('hide')) {
+          loadMoreBtn.classList.remove('hide');
+          footerContent.classList.remove('hide');
+          header.classList.remove('alwaysShow');
         }
-        const loadMoreBtn = document.querySelector('.loadMoreBtn');
-        const footerContent = document.querySelector('.footer__content');
-        const header = document.querySelector('.header');
-        if (lastPage * IMAGES_PER_PAGE >= response.data.totalHits) {
-          loadMoreBtn.classList.add('hide');
-          footerContent.classList.add('hide');
-          header.classList.add('alwaysShow');
-          Notify.info(
-            "We're sorry, but you've reached the end of search results."
-          );
-        } else {
-          if (loadMoreBtn.classList.contains('hide')) {
-            loadMoreBtn.classList.remove('hide');
-            footerContent.classList.remove('hide');
-            header.classList.remove('alwaysShow');
-          }
-        }
-        gallery.updatePageCount();
-        if (
-          !Array.isArray(response.data.hits) ||
-          response.data.hits.length < 1
-        ) {
-          Notify.failure(
-            'Sorry, there are no images matching your search query. Please try again.'
-          );
-        }
-        gallery.renderCards(response.data.hits);
-      })
-      .catch(function (error) {
-        // handle error
+      }
+      gallery.updatePageCount();
+      if (!Array.isArray(response.data.hits) || response.data.hits.length < 1) {
         Notify.failure(
-          'Oops, something went wrong communicating with the server.. Please try again later.'
+          'Sorry, there are no images matching your search query. Please try again.'
         );
-        console.log(error);
-      })
-      .finally(function () {
-        // always executed
-      });
-
-    //
+      }
+      gallery.renderCards(response.data.hits);
+    } catch (error) {
+      Notify.failure(
+        'Oops, something went wrong communicating with the server.. Please try again later.'
+      );
+      console.log(error);
+    } finally {
+      // always executed
+    }
   } else {
     gallery.clear();
   }
